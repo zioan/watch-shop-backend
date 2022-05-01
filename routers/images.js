0bcf4d8c31ae700360977735f8088d00df85a249
zioan <zaharia.ioan@gmail.com>
2022-05-01 13:30:24 +0200

    image delete route

--- routers/images.js
+++ routers/images.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
+const fs = require('fs');
 
 router.post('/upload', (req, res) => {
   const newpath = __dirname + '/files/';
@@ -13,6 +14,14 @@ router.post('/upload', (req, res) => {
     }
     res.status(200).send({ message: 'File Uploaded', code: 200 });
   });
+});
+
+router.delete('/delete/:path', async (req, res) => {
+  try {
+    fs.unlinkSync(`./routers/files/${req.params.path}`);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
